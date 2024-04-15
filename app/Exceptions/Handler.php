cef291b15aa4454ba05b018b02c5353b290cfda9
rodrigojustinodevs <rodrigojustino.devs@gmail.com>
2024-04-15 18:08:15 -0300

    implementation design pattern and settings

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,15 @@
 
 namespace App\Exceptions;
 
+use App\Http\Responses\ApiResponse;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +51,70 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (NotFoundHttpException $e) {
+            return ApiResponse::error(
+                [
+                    'exception' => $e->getMessage()
+                ],
+                'Rota não encontrada', Response::HTTP_NOT_FOUND);
+        });
+
+        $this->renderable(function (MethodNotAllowedHttpException $e) {
+            return ApiResponse::error(
+                $e->getMessage(),
+                'Método HTTP não permitido',
+                Response::HTTP_METHOD_NOT_ALLOWED
+            );
+        });
+
+        $this->renderable(function (AuthenticationException $e) {
+            return ApiResponse::error(
+                $e->getMessage(),
+                'Usuário não autenticado',
+                Response::HTTP_UNAUTHORIZED
+            );
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e) {
+            return ApiResponse::error(
+                $e->getMessage(),
+                'Acesso negado',
+                Response::HTTP_FORBIDDEN
+            );
+        });
+
+        $this->renderable(function (ValidationException $e) {
+            $errors = $e->errors();
+            $firstError = $errors[array_key_first($errors)][0];
+            return ApiResponse::error(
+                $e->errors(),
+                $firstError,
+                Response::HTTP_UNPROCESSABLE_ENTITY,
+                true
+            );
+        });
+
+        $this->renderable(function (Throwable $exception, Request $request) {
+            $debug = env('APP_DEBUG')
+                ? [
+                    'message' => $exception->getMessage(),
+                    'line'    => $exception->getLine(),
+                    'code'    => $exception->getCode(),
+                    'trace'   => $exception->getTrace(),
+                ]
+                : [];
+
+            $data = [
+                'exception' => $exception->getMessage(),
+                'request'   => $request->fullUrl(),
+                'debug'     => $debug,
+            ];
+
+            return ApiResponse::error(
+                $data,
+                'Erro interno no servidor',
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
         });
     }
 }
